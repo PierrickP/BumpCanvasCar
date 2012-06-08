3c760dada3b0ca57351cf79bfffb0aa6c2feb6fb
Pierrick Paul <mulotmail@gmail.com>
2012-06-08 15:26:52 +0800

    Init template and start form
    Setup twitter bootstrap and express. Start to create form for
    registration

--- server.js
+++ server.js
@@ -13,7 +13,70 @@ fs = require('fs');
   
 var players = [];
 
-var deleteplayer = function (n) {
+/*
+*  Init and config
+*/
+
+io.configure(function () {
+    //io.set("transports", ["websocket"]);
+    io.set("polling duration", 3);
+});
+
+var locals = {
+    title: 		 'BumpCanvasCar',
+    description: 'A simple multiplayer Bump Car game',
+    author: 	 'Pierrick PAUL'
+};
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express['static'](__dirname + '/static'));
+    app.use(app.router);
+    app.enable("jsonp callback");
+});
+
+app.configure('development', function(){
+   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    // app.use(express.logger({ format: ':method :url' }));
+});
+
+app.configure('production', function(){
+   app.use(express.errorHandler()); 
+});
+
+
+app.error(function(err, req, res, next){
+   res.render('500.ejs', { locals: { error: err },status: 500 });	
+});
+
+/*
+*  Routing
+*/
+
+app.get('/', function (req, res) {
+    locals.date = new Date().toLocaleDateString();
+    res.render('index.ejs', locals);
+});
+
+app.get('/registration', function(req, res){
+    res.render('registration.ejs', locals);
+});
+
+app.post('/registration', function(req, res){
+  console.log(req.body.user);
+  //res.redirect('/');
+});
+
+app.get('/*', function(req, res){
+    res.render('404.ejs', locals);
+});
+
+/* Game */
+
+function deleteplayer(n) {
     var t = [];
     for (var i = 0; i < players.length; i++) {
         if (players[i].name !== n) {
@@ -21,20 +84,7 @@ var deleteplayer = function (n) {
         }
     }
     return t;
-};
-
-app.configure(function () {
-    app.use(express['static'](__dirname + '/client'));
-});
-
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/client/index.html');
-});
-
-io.configure(function () {
-    //io.set("transports", ["websocket"]);
-    io.set("polling duration", 3);
-});
+}
 
 function Player(s, n) {
     this.socket = s.id;
@@ -77,3 +127,4 @@ io.sockets.on('connection', function (socket) {
 });
 
 app.listen(process.env['app_port'] || 3000);
+console.log("Listening on port %d in %s mode", app.address().port, app.settings.env);
